c07aff96186b2389bb503fd616da8014366eca47
rnampell <rajashekar.nampelli@gmail.com>
2018-12-01 12:13:27 -0800

    first commit

---
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -0,0 +1,51 @@
+package com.amazonaws.lambda.demo;
+
+import java.io.InputStream;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+public class LambdaFunctionHandler implements RequestHandler<S3Event, String> {
+
+    private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+
+    public LambdaFunctionHandler() {}
+
+    // Test purpose only.
+    LambdaFunctionHandler(AmazonS3 s3) {
+        this.s3 = s3;
+    }
+
+    @Override
+    public String handleRequest(S3Event event, Context context) {
+    	
+        context.getLogger().log("Received event: " + event);
+        context.getLogger().log("My Custom Lambda FUnction Invoked");
+        
+        // Get the object from the event and show its content type
+        String bucket = event.getRecords().get(0).getS3().getBucket().getName();
+        String key = event.getRecords().get(0).getS3().getObject().getKey();
+        try {
+            S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
+            String contentType = response.getObjectMetadata().getContentType();
+            context.getLogger().log("CONTENT TYPE: " + contentType);
+            S3Object xFile = s3.getObject(bucket, "app.js");
+            InputStream contents = xFile.getObjectContent();
+            context.getLogger().log("COntents " + contents);
+            return contentType;
+        } catch (Exception e) {
+            e.printStackTrace();
+            context.getLogger().log(String.format(
+                "Error getting object %s from bucket %s. Make sure they exist and"
+                + " your bucket is in the same region as this function.", key, bucket));
+            throw e;
+        }
+    }
+
+}
